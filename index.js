8ebd586420696b9747d7c461c92f5493725fec29
Alex Artemev <bartleby.fla@gmail.com>
2021-08-17 14:02:34 +0700

    refactor: Добавлен мок процесса

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const core = require('@actions/core');
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 const {generateReleaseNotes} = require('./build-release');
+
+let process = {env: {}};
+process.env.GITHUB_REF = "undefined";
+
