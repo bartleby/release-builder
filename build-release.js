c5ab751d002e3c0a62d9acab79896ac9fd27c067
Alex Artemev <hello@idevs.io>
2021-08-17 13:55:25 +0700

    Feature/test feature (#1)
    * refactor: добавлена зависимость child_process
    * feat: добавлена зависимость util
    * feat: Добавлен релиз мейкер
    
    * feat: Добавлены константы

---
+++ build-release.js
@@ -0,0 +1,49 @@
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
+const repoURL = 'repo url'//process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY;
+async function generateReleaseNotes({range, dateFormat = 'short'}) {
+    dateFormat = dateFormat.includes('%') ? 'format:' + dateFormat : dateFormat;
+    // Get commits between computed range
+    let {stdout: commits} = await execFile('git', [
+        'log',
+        '--format=%H¬%ad¬%s¬%b',
+        '--date=' + dateFormat,
+        range
+    ].filter(Boolean));
+    commits = commits.split('\n').filter(Boolean).map(line => {
+        const [hash, date, title, body] = line.split('¬');
+        return {
+            hash: hash.slice(0, 8),
+            date,
+            title,
+            body
+        };
+    });
+
+    const commitEntries = [];
+
+    for (const {hash, date, title, body} of commits) {
+        let bodyCommits = body.split("\n").filter(Boolean)
+        let regExp = RegExp("/" +bodyCommits.map(item => {"^" + item + "\s|"}).join("") + "^feat:\s" + "/i")
+        let list = bodyCommits.filter(({title}) => !regExp.test(title))
+
+        commitEntries.push({
+            title: title,
+            list: list
+        });
+
+        console.log("hash: " + hash + ", date: " + date + ", title: " + title + ", body: " + body);
+    }
+
+
+
+    console.log(commitEntries.map(item => {return "title: " + item.title + ", body: [" + item.list.join(", ") + "]"}))
+    //
+
+    return commitEntries.map(item => {return item.title}).filter(Boolean).join('\n');
+    //return releaseTemplate
+      //  .replace('{commits}', commitEntries.join('\n'))
+        //.replace('{range}', `[\`${range}\`](${repoURL}/compare/${range})`);
+}
+
+exports.generateReleaseNotes = generateReleaseNotes;
