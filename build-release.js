a7bf7494466006b954be78a35279f3e2148e7366
Alex Artemev <bartleby.fla@gmail.com>
2021-08-20 14:35:57 +0700

    feat: commit parser

--- build-release.js
+++ build-release.js
@@ -1,56 +1,133 @@
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
-const repoURL = 'repo url'//process.env.GITHUB_SERVER_URL + '/' + process.env.GITHUB_REPOSITORY;
-const commitsTags = ["feat:", "fix:", "perf:"]
-const commitsTagTitles = [
-    {"feat:": "Новый функционал"},
-    {"fix:": "Исправлены ошибки"},
-    {"perf:": "Улучшина производительность"}
-]
+const commitKeys = {
+    "feat": "Новый функционал:",
+    "fix": "Исправлены ошибки:",
+    "perf": "Улучшена производительность:",
+    "refactor": "Рефакторинг кода:",
+    "other": "Другие работы:"
+}
 
-async function generateReleaseNotes({range, dateFormat = 'short'}) {
+let groupedCommits = []
+
+async function generateReleaseModel({range, dateFormat = 'short'}) {
     dateFormat = dateFormat.includes('%') ? 'format:' + dateFormat : dateFormat;
     // Get commits between computed range
     let {stdout: commits} = await execFile('git', [
         'log',
-        '--format=%H¬%ad¬%s¬%b',
+        '--format=%s|%h|%ad|%b(^)',
         '--date=' + dateFormat,
         range
     ].filter(Boolean));
-    commits = commits.split('\n').filter(Boolean).map(line => {
-        const [hash, date, title, body] = line.split('¬');
+    commits = commits.split("(^)").filter(Boolean).map(line => {
+        const commit = line.split('|')
+        const [title, hash, date, body] = commit
+
         return {
-            hash: hash.slice(0, 8),
-            date,
-            title,
-            body
+            hash: hash,
+            date: date,
+            title: title,
+            body: body
         };
     });
 
-    const commitEntries = [];
-
     for (const {hash, date, title, body} of commits) {
-        let bodyCommits = body.split("\n").filter(Boolean)
-        let regExp = RegExp("/" +bodyCommits.map(item => {"^" + item + "\s|"}).join("") + "^feat:\s" + "/i")
-        let list = bodyCommits.filter(({title}) => !regExp.test(title))
+        if (body === undefined) {continue}
 
-        commitEntries.push({
-            title: title,
-            list: list
-        });
+        let commitTitle = removeNewLineSymbols(title)
+        let commitKey = keyFromCommit(commitTitle)
+        const regExp = makeRegExp(commitKey)(false, "", true)
+        const commitSubject = commitTitle.replaceAll(regExp, "")
 
-        console.log("hash: " + hash + ", date: " + date + ", title: " + title + ", body: " + body);
+        let bodyCommits = body
+            .split("\n")
+            .filter(i => {return "" !== i})
+            .filter(i => {return "\r" !== i})
+        const otherKey = "other"
+        if (bodyCommits.length === 0) {
+            let groupIndex = getIndexFor(otherKey, groupedCommits)
+
+            if (groupIndex === -1) {
+                groupedCommits.push({key: otherKey, title: commitKeys[otherKey], list: []})
+                groupIndex = groupedCommits.length - 1
+            }
+
+            if (commitKey === "none") {continue}
+
+            let index = getIndexFor(commitKey, groupedCommits[groupIndex].list)
+            if (index === -1) {
+                groupedCommits[groupIndex].list.push({key: commitKey, title: commitKeys[commitKey], list: []})
+                index = groupedCommits[groupIndex].list.length - 1
+            }
+            groupedCommits[groupIndex].list[index].list.push(commitSubject)
+
+            continue
+        }
+
+        for (const rawBodyCommitTitle of bodyCommits) {
+            const bodyCommitTitle = removeNewLineSymbols(rawBodyCommitTitle)
+            const bodyCommitKey = keyFromCommit(bodyCommitTitle)
+
+            if (bodyCommitKey === "none") {
+                continue
+            }
+
+            const bodyCommitRegExp = makeRegExp(bodyCommitKey)(false, "\\* ", true)
+            const bodyCommitSubject = bodyCommitTitle.replaceAll(bodyCommitRegExp, "")
+            let groupIndex = getIndexFor(commitTitle, groupedCommits)
+
+            if (groupIndex === -1) {
+                groupedCommits.push({key: commitTitle, title: commitTitle, list: []})
+                groupIndex = groupedCommits.length - 1
+            }
+
+            let bodyCommitKeyIndex = getIndexFor(bodyCommitKey, groupedCommits[groupIndex].list)
+            if (bodyCommitKeyIndex === -1) {
+                groupedCommits[groupIndex].list.push({key: bodyCommitKey, title: commitKeys[bodyCommitKey], list: []})
+                bodyCommitKeyIndex = groupedCommits[groupIndex].list.length - 1
+            }
+            groupedCommits[groupIndex].list[bodyCommitKeyIndex].list.push(bodyCommitSubject)
+        }
     }
-
-
-
-    console.log(commitEntries.map(item => {return "title: " + item.title + ", body: [" + item.list.join(", ") + "]"}))
-    //
-
-    return commitEntries.map(item => {return item.title}).filter(Boolean).join('\n');
-    //return releaseTemplate
-    //  .replace('{commits}', commitEntries.join('\n'))
-    //.replace('{range}', `[\`${range}\`](${repoURL}/compare/${range})`);
+    return groupedCommits
 }
 
-exports.generateReleaseNotes = generateReleaseNotes;
+function makeRegExp(key = "") {
+    return function (mustBegin = true, prefix = "", isGlobal = false) {
+        let start = mustBegin ? "\^" : ""
+        let flags = isGlobal ? "ig" : "i"
+        return RegExp(start + prefix + key + ": ", flags)
+    }
+}
+
+function removeNewLineSymbols(value = "") {
+    return value
+        .replaceAll("\n", "")
+        .replaceAll("\r", "")
+        .trim()
+}
+
+function keyFromCommit(commit = "") {
+    for (let key in commitKeys) {
+        let reqExp = makeRegExp(key)
+        if (reqExp().test(commit)) {
+            return key
+        }
+        if (reqExp(true, "\\* ").test(commit)) {
+            return key
+        }
+    }
+    return "none"
+}
+
+function getIndexFor(key = "", inArray = []) {
+    for (const i in inArray) {
+        if (inArray[i].key === key) {
+            return i
+        }
+    }
+    return -1
+}
+
+
+exports.generateReleaseModel = generateReleaseModel;
--- index.js
+++ index.js
@@ -2,7 +2,34 @@ const core = require('@actions/core');
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 const {generateReleaseNotes} = require('./build-release');
+const {generateHtmlRelease} = require("./html-format-builder");
+const {generateSlackRelease} = require("./slack-format-builder");
+const {slackTemplate} = require("./slack-template");
 
 let process = {env: {}};
 process.env.GITHUB_REF = "undefined";
 
+        // parser release
+        const changelog = await generateReleaseModel({range});
+        const htmlRelease = await generateHtmlRelease(changelog);
+        const slackRelease = await generateSlackRelease(changelog)
+
+        const slackChangelog = slackTemplate(
+            channel,
+            header,
+            projectName,
+            buildUrl,
+            releaseString,
+            pushedTag,
+            qrCodeUrl,
+            slackRelease
+        )
+
+        core.setOutput('changelog-html', htmlRelease);
+        core.setOutput('changelog-slack', slackChangelog);
+        core.setOutput('tag-name', pushedTag);
+
+        console.log('changelog-release: ' + htmlRelease)
+        console.log('changelog-slack: ' + slackChangelog)
+
+        core.info('Created changelog:\n`' + htmlRelease);
