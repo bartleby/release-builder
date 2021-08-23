d997676da07f9fabe3878c27a17a090ed2458f5b
Alex Artemev <bartleby.fla@gmail.com>
2021-08-23 14:55:17 +0700

    fix: slack block limit

--- slack-format-builder.js
+++ slack-format-builder.js
@@ -1,6 +1,7 @@
 const headerPattern = "{\"type\": \"header\",\"text\": {\"type\": \"plain_text\",\"text\": \"{header}\",\"emoji\": true}},"
 const sectionPattern = "{\"type\": \"section\",\"text\": {\"type\": \"mrkdwn\",\"text\": \"{subject}\"}},"
 
+const reserved_block_count = 10
 const slack_blocks_limit = 50
 function getHeader(title = "") {
     return headerPattern.replace(RegExp("{header}", "ig"), title)
@@ -15,7 +16,7 @@ async function generateSlackRelease(model) {
         return getHeader(item.title) + item.list.map( subtitle => {
             return getSection("*" +subtitle.title + "*" + "\\n" + subtitle.list.join("\\n"))
         }).join("")
-    }).slice(model.length - slack_blocks_limit, model.length).join("")
+    }).split(0, slack_blocks_limit - reserved_block_count).join("")
 }
 
 exports.generateSlackRelease = generateSlackRelease;
